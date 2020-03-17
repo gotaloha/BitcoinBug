64fd2fc8d48eec3813b4f59abc63673747067fd5
Erik Olson <erik@gotaloha.com>
2020-03-16 20:54:22 -0500

    More refinement and stuff.
    - src/index.css: Removed "both" from the animation declaration (Is that a new band name?)
    - src/render.js: Refactored

--- src/render.js
+++ src/render.js
@@ -1,44 +1,28 @@
 const { desktopCapturer, remote } = require('electron');
 const { dialog, Menu } = remote;
 
-// Global variables
+/********************************************************************************
+ * Respect the API owner: don't call this service excessively
+ ********************************************************************************/
 
-const delay = 1000 * 60 * 5; // 5 minutes (1000 milliseconds in a second * 60 seconds in a minute * how many desired minutes)
+let url = 'https://api.coindesk.com/v1/bpi/currentprice.json',
+    delay = 1000 * 60 * 5, // 5 minutes
+    placeholder = 'price'; // Id of placeholder div
 
 const callAPI = async (url) => {
   const getAPI = await fetch(url)
     .then((response) => response.json())
     .then(data => {
       updatePrice(data.bpi.USD.rate_float);
-  })(url);
-
-  getAPI(url); // Call the API on load
-
-  setTimeout(getAPI(url), delay);
+  });
 }
+
+setInterval(() => {
+  callAPI(url);
+}, delay);
 
 const updatePrice = (num) => {
-  countdownTimer();
-  document.getElementById('price').innerHTML = '$' + num.toFixed(2);
+  document.getElementById(placeholder).innerHTML = '$' + num.toFixed(2);
 }
 
-const countdownTimer = () => {
-  console.log("countdownTimer run");
-  let timer = 60 * 5,
-      total = timer;
-
-  const countdown = () => {
-    if (timer <= 1) {
-      clearInterval(this.timer);
-      timer = total;
-    } else {
-      timer--;
-    }
-  }
-
-  setInterval(function() {
-    countdown();
-  }, 1000);
-}
-
-callAPI('https://api.coindesk.com/v1/bpi/currentprice.json');
+callAPI(url);
